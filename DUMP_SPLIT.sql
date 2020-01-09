b909ba48e00513b1d40d65497c6f7ece90ae9a05
nhamct <caothinham@gmail.com>
2020-01-09 15:29:51 +0700

    aslkjdf;lákdf

---
+++ DUMP_SPLIT.sql
@@ -0,0 +1,7 @@
+﻿
+update fckdump set ten = trim(ten)
+
+select right (ten, CHARINDEX(' ',reverse(ten)) - 1) from fckdump --cắt tên
+
+select left(ten, len(ten) - CHARINDEX(' ',reverse(ten))) ten1 from fckdump -- cắt họ và tên đệm
+
